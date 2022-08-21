3b82ef9d52933756ecb9bb1b33a5440999abff0d
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-21 10:47:18 +0530

    Add files via upload

--- 21stAugust.py
+++ 21stAugust.py
@@ -1,22 +1,22 @@
-print("Hello World!! This is my first program")
-print("Hey! How are you?")
+# print("Hello World!! This is my first program")
+# print("Hey! How are you?")
 
 # end
 
-print("Hello",end=" ")
-print('World')
+# print("Hello",end=" ")
+# print('World')
 
-print("Hello",end="\t")
-print('World')
+# print("Hello",end="\t")
+# print('World')
 
-print("Hello",end="\n")
-print('World')
+# print("Hello",end="\n")
+# print('World')
 
-print("Hello",end="Nakshi")
-print('World')
+# print("Hello",end="Nakshi")
+# print('World')
 
-print("Hello",end="")
-print('World')
+# print("Hello",end="")
+# print('World')
 
 """
 Escaping Characters/Sequences
@@ -28,12 +28,35 @@ Escaping Characters/Sequences
 \ ---> backslash
 """
 
-print("Good\nMorning")
+# print("Good\nMorning")
 
-print("Good\tMorning")
+# print("Good\tMorning")
 
-print("Good\bMorning")
+# print("Good\bMorning")
 
-print("Python is a Fun\rJava")
+# print("Python is a Fun\rJava")
 
-print("Python is a Fun\r1234567")
+# print("Python is a Fun\r1234567")
+
+"""
+Hey\namaste\thanks\bye
+"""
+
+# print("Hey\ \bnamaste\ \bthanks\ \bbye")
+
+# print("Hey\\namaste\\thanks\\bye")
+
+"""
+Nakshi Said: "Hello World"
+"""
+print('Nakshi Said: "Hello World"')
+
+print("Nakshi Said: \"Hello World\"")
+
+"""
+Nakshi :'Good Morning'
+"""
+
+print("Nakshi :'Good Morning'")
+
+print('Nakshi :\'Good Morning\'')
