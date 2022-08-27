4aa890c4c3812a67482d28043610307d55997048
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-27 11:46:17 +0530

    Add files via upload

---
+++ Numeric.py
@@ -0,0 +1,36 @@
+"""
+Numeric Data Types
+
+1. Integer
+2. Float
+3. Complex
+
+---> Python is an implicit bind language.
+"""
+
+a = 10
+
+print("Type of variable a is: ",type(a))
+print("a: ",a)
+
+b = 1954561321321231231231654561
+
+print("\nType of variable b is: ",type(b))
+print("b: ",b)
+
+
+c = 89.98
+
+print("\n\nType of the variable c is: ",type(c))
+print("c: ",c)
+
+d = 48798.986512312313218541212312
+
+print("\nType of variable d is: ",type(d))
+print("d: ",d)
+
+
+e = 2 + 3j
+
+print("\n\nType of variable e is: ",type(e))
+print("e: ",e)
