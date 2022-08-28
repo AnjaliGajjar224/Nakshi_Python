33b291d9a0af5490b4372785f95e9d6f2aa399bf
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-28 11:59:08 +0530

    Add files via upload

--- Arithmetic.py
+++ Arithmetic.py
@@ -8,13 +8,48 @@ Arithmetic Operators:
 6. // (floor division)
 7. ** (Exponent)
 """
-a = 15
-b = 10
+# a = 15
+# b = 10
 
-print("Addition: ",a+b)
-print("Subtraction: ",a-b)
-print("Multiplication: ",a*b)
-print("Division: ",a/b)
-print("Remainder: ",a%b)
-print("Floor division: ",a//b)
-print("Exponent: ",a**b)                   # 5 ** 2 ---> base ** power ---> 5 ^ 2 = 25
+# print("Addition: ",a+b)
+# print("Subtraction: ",a-b)
+# print("Multiplication: ",a*b)
+# print("Division: ",a/b)
+# print("Remainder: ",a%b)
+# print("Floor division: ",a//b)
+# # print("Exponent: ",a**b)                   # 5 ** 2 ---> base ** power ---> 5 ^ 2 = 25
+
+# print("Enter number 1: ")
+
+# num1 = input()                    # by default it take strings
+
+# print(type(num1))
+
+# num2 = input("Enter number 2: ")
+# print(type(num2))
+
+
+# print("Addition ",num1+num2)
+
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+# print("Addition: ",num1+num2)
+
+
+"""
+Take two numbers from the user and print Addition, Subtraction , Multiplication , Division, Remainder, Floor Division, Exponent of it.
+Input:
+------
+a = 15
+b = 5
+
+Output:
+--------
+15 + 5 = 20
+15 - 5 = 10
+15 * 5 = 75
+15 / 5 = 3.0
+15 % 5 = 0
+15 // 5 = 3
+15 ** 5 = 759375
+"""
