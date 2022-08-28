6932e3d44d687215356f485bed9fabff91272ffa
AnjaliGajjar224 <98864149+AnjaliGajjar224@users.noreply.github.com>
2022-08-28 11:34:11 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,20 @@
+"""
+Arithmetic Operators:
+1. + (Addition)
+2. - (Subtraction)
+3. * (Multiplication)
+4. / (Division)
+5. % (Remainder)
+6. // (floor division)
+7. ** (Exponent)
+"""
+a = 15
+b = 10
+
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
+print("Remainder: ",a%b)
+print("Floor division: ",a//b)
+print("Exponent: ",a**b)                   # 5 ** 2 ---> base ** power ---> 5 ^ 2 = 25
